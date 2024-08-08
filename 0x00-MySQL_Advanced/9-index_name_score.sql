560e1d5a11a3ae88e09b7beac801dde3b731f032
joel magana <joelmagana15@gmail.com>
2024-08-08 14:17:32 +0000

    9 check

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,4 @@
 -- script that create an index idx_name_first_score on
 --table names and first letter of name and the score
+DROP INDEX IF EXISTS idx_name_first_score ON names;
 CREATE INDEX idx_name_first_score ON names (SUBSTRING(name, 1, 1) score );
